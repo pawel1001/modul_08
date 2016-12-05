c29bd4dcdcadd17223eb03cd7522b98e31062633
pawel <pawel1001@vp.pl>
2016-12-05 18:59:02 +0100

    Triangle

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-var name = prompt('Wpisz swoje imiê');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = 1;
+var h = 3;
+var triangleArea = a*h/2;
+
+a = prompt('Wpisz wartosc podstawy trojkat');
+h = prompt('A teraz podaj wysokosc');
+alert('Pole trojkata o podstawie a: ' + a + ' i wysokosci h: ' + h + ' wynosi ' + triangleArea);
+console.log('Pole trojkata o podstawie a: ' + a + ' i wysokosci h: ' + h + ' wynosi ' + triangleArea);
