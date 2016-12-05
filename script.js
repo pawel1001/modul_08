bcfdd3793ad992e8024aee8f26309572c6e1f0f6
pawel <pawel1001@vp.pl>
2016-12-05 22:29:49 +0100

    08_09

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-var name = prompt('Wpisz swoje imi�');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = 1;
+var b = 1;
+
+function rysujChoinke (a) {
+	var star = "";
+
+	for (var k = 0; k < a; k++) {
+	star += '*';
+	console.log(star)
+	}	
+	
+}
+
+rysujChoinke(3);
+rysujChoinke(5);
+
+//Dla ambitnych: Na każdym z poziomów dodaj spacje tak, aby choinka stała prosto.
+
+function rysujChoinke2(b) {
+	
+	var star = '';
+
+	for (var k = 0; k < 2*b; k++) {
+	
+	if (k==0) {
+		star+='*';
+	} else {
+			star += '**';
+		}
+
+	
+	var space = '';
+
+		for (var n = k; n <2*b-1; n++) {
+			space += ' ';
+		}
+	console.log(space + star)
+	}	
+	
+
+}
+
+rysujChoinke2(5);
