37c8814be30f56537409445f02e2ae51855281a9
pawel <pawel1001@vp.pl>
2016-12-05 19:12:38 +0100

    modul_08_05

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var name = prompt('Wpisz swoje imiê');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = 1;
+var b = 1;
+var value = (a*a) + (2 * a * b) - (b*b);
+a = prompt('Podaj wartosc a');
+b = prompt('Podaj wartosc b');
+alert('Wynik kalkulacji to: ' + value);
+console.log(value);
+if (value >=0) {
+	console.log('Wynik dodatni!');
+} else if (value < 0) {
+	console.log('Wynik ujemny!');
+} else {
+	console.log('Wynik rowny zero!');
+}
