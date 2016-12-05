f4246a4f9cf67e1d5b32ee6e98c2fb0e33a2912b
pawel <pawel1001@vp.pl>
2016-12-05 22:34:21 +0100

    8_11

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-var name = prompt('Wpisz swoje imiê');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var licznik = 0;
+var addItem = document.getElementById('addItem');
+	
+addItem.addEventListener('click', function(e) {
+  licznik += 1;
+var list = document.getElementById('lista');
+var newElement = document.createElement('li');
+newElement.innerHTML = 'Item ' + licznik;
+list.appendChild(newElement);
+});
+
