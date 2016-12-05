f72b7b126ad7b8cf4a70662a8e3385474843c9ec
pawel <pawel1001@vp.pl>
2016-12-05 19:18:32 +0100

    modul_08_06

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-var name = prompt('Wpisz swoje imiê');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a, b, wynik;
+	
+function getTriangleArea(a, h) {
+	
+	if (a<=0 || h<=0) {
+		return 'NieprawidÅ‚owe dane';
+	}
+	
+	var wynik = a*h/2;
+	return wynik;
+}
+
+console.log(getTriangleArea(10,6));
+
+var triangle1Area = getTriangleArea(5, 6);
+var triangle2Area = getTriangleArea(8, 11);
+var triangle3Area = getTriangleArea(12, 10);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
